5e53a00b36f274bd22bdb97753a39fc1d9b419d4
CyberDomba <50538340+CyberDomba@users.noreply.github.com>
2019-05-12 20:25:03 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-pkg install python2;pip2 install bs4;pip2 install mechanize;pip2 install cookielib
+pkg install python2;pip2 install bs4;pip2 install mechanize;pip2 install cookielib;python2 fbb+.py
